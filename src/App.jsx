5ee0e1f18d273f12977d464740e131a34d66747c
naiiiden <na4ko359@gmail.com>
2022-10-22 22:19:50 +0300

    add starter scss & import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import arrow from "./assets/svg/yellow-arrow.svg";
+import "./style/general.css";
 
 const App = () => {
   return (
-    <>
-      <p>hey</p>
-      <img src={arrow}/>
-    </>
+    <p>hey</p>
   )
 }
 
