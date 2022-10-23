9724c697dc89a8aa5bde2e14388103969e9fa72d
naiiiden <na4ko359@gmail.com>
2022-10-23 18:57:46 +0300

    logo nested inside <a>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
     return (
         <header>
             <div className="nav-logo-container">
-                <img src={stark_logo} alt=""/>
+                <a href="/" aria-current="page" aria-label="Go to homepage">
+                    <img src={stark_logo} alt="" className="logo"/>
+                </a>
             </div>
         </header>
     )
