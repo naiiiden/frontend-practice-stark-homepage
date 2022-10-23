3f1fd00567eb5f5ed6d94d22e61457825020acf8
naiiiden <na4ko359@gmail.com>
2022-10-23 21:58:35 +0300

    header component add rest of links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,24 @@ const Header = () => {
                 <a href="/" aria-current="page" aria-label="Go to homepage">
                     <img src={stark_logo} alt="" className="logo"/>
                 </a>
+                <ul className="nav-ul">
+                    <li>
+                        <span>PRODUCT</span>
+                        <ul className="nav-product-submenu">
+                            <li><a href="">Stark for Mac</a></li>
+                            <li><a href="">Stark For Figma</a></li>
+                            <li><a href="">Stark for Sketch</a></li>
+                            <li><a href="">Stark for Adobe XD</a></li>
+                            <li><a href="">Stark for Chrome</a></li>
+                            <li><a href="">Stark for Brave</a></li>
+                            <li><a href="">Stark for Edge</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">PRICING</a></li>
+                    <li><a href="">BLOG</a></li>
+                    <li><a href="">LIBRARY</a></li>
+                    <li><a href="">SUPPORT</a></li>
+                </ul>
                 <a href="" className="nav-link nav-link--sign-up">30-DAY FREE TEAM TRIAL</a>
             </nav>
             <button onClick={() => setOpenMenu(!openMenu)} aria-label={!openMenu ? "Open menu" : "Close menu"} aria-expanded={!openMenu ? "false" : "true"} aria-haspopup="true">
