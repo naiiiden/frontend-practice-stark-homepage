5307bda30df9663ca26121c8de2f71d6ae40ea63
naiiiden <na4ko359@gmail.com>
2022-10-23 20:33:38 +0300

    style mobile menu button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import stark_logo from "../assets/svg/logo.svg";
 const Header = () => {
     return (
         <header>
-            <div className="nav-logo-container">
+            <nav className="nav-logo-container">
                 <a href="/" aria-current="page" aria-label="Go to homepage">
                     <img src={stark_logo} alt="" className="logo"/>
                 </a>
-            </div>
+            </nav>
+            <button>
+                <div className="hamburger"></div>
+            </button>
         </header>
     )
 }
