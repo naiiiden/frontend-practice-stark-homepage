8da1164b6ef428bb4a9ef051255c69ee119d5c97
naiiiden <na4ko359@gmail.com>
2022-10-29 22:42:01 +0300

    header render arrow on 1024px+

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
 import Nav from "./Nav";
+import yellow_arrow from "../assets/svg/yellow-arrow.svg";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+    const [isDesktop, setIsDesktop] = useState(false);
+    
+    const updateMedia = () => setIsDesktop(window.innerWidth <= 1023);
+    
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
     return (
         <header>
             <Nav/>
@@ -9,9 +20,12 @@ const Header = () => {
                     <p className="introduction">INTRODUCING THE ALL-NEW STARK SUITE</p>
                     <h1>Super­charge Access­ibility</h1>
                     <p className="description">Tired of manually checking spreadsheets, messy handovers, and expensive retrofitting? Stark connects the tools you and your team already use in a streamlined end-to-end accessibility workflow you actually love.</p>
-                    <div className="links-wrapper">
-                        <a href="" className="free">Create a free account</a>
-                        <a href="" class="learn">Learn more</a>
+                    <div className="links-arrow-container">
+                        <div className="links-wrapper">
+                            <a href="" className="free">Create a free account</a>
+                            <a href="" className="learn">Learn more</a>
+                        </div>
+                        {!isDesktop && <img src={yellow_arrow} alt=""/>}
                     </div>
                 </div>
             </div>
