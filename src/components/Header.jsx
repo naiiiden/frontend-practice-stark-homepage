496320c521c5e89fc90592fc4c85fe9b649c92ea
naiiiden <na4ko359@gmail.com>
2022-10-29 11:11:34 +0300

    add text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,17 @@ const Header = () => {
     return (
         <header>
             <Nav/>
+            <div className="text-illustration-wrapper">
+                <div className="text-container">
+                    <p className="introduction">INTRODUCING THE ALL-NEW STARK SUITE</p>
+                    <h1>Super­charge Access­ibility</h1>
+                    <p className="description">Tired of manually checking spreadsheets, messy handovers, and expensive retrofitting? Stark connects the tools you and your team already use in a streamlined end-to-end accessibility workflow you actually love.</p>
+                    <div className="links-wrapper">
+                        <a href="" className="free">Create a free account</a>
+                        <a href="" class="learn">Learn more</a>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
