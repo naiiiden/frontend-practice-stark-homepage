8e5be4dbfcdc3416961438fa0ebabbb1910ec7d1
naiiiden <na4ko359@gmail.com>
2022-10-31 22:22:52 +0200

    video container component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,24 @@
+import yellow_bg from "../assets/svg/yellow-bg.svg";
+import play from "../assets/svg/play.svg";
+import video_placeholder from "../assets/images_homepage/video-still.webp";
+
 const Video = () => {
     return (
-        <section>
-            <h2>
-                Speed up your accessibility
-                <br/>
-                workflow from months to minutes
-            </h2>
+        <section className="video-section">
+            <div className="h2-svg-container">
+                <h2>
+                    Speed up your accessibility
+                    <br/>
+                    workflow from months to minutes
+                </h2>
+                <img src={yellow_bg} alt=""/>
+            </div>
+            <div className="video-container">
+                <img src={video_placeholder} alt="" />
+                <img src={play} alt=""/>
+                {/* embed youtube video after styling */}
+            </div>
+            <p>More than 40,000 designers, developers, product managers, and accessibility experts from over 12,000 companies use Stark’s suite of integrated accessibility tools in their daily product development cycle. Join them today!</p>
         </section>
     )
 }
