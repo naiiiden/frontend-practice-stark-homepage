a648d38a6e9098881898ff6f6f0c12582226066c
naiiiden <na4ko359@gmail.com>
2022-11-02 20:49:55 +0200

    customers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./style/general.css";
 import Header from "./components/Header";
 import Video from "./components/Video";
+import Customers from "./components/Customers";
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Video/>
+      <Customers/>
     </div>
   )
 }
---
+++ src/components/Customers.jsx
@@ -0,0 +1,89 @@
+import charles_schwab from "../assets/svg_customers/charles_schwab.svg";
+import us_bank from "../assets/svg_customers/us_bank.svg";
+import united_airlines from "../assets/svg_customers/united_airlines.svg";
+import shopify from "../assets/svg_customers/shopify.svg";
+import mission_lane from "../assets/svg_customers/mission_lane.svg";
+import american_express from "../assets/svg_customers/american_express.svg";
+import fedex from "../assets/svg_customers/fedex.svg";
+import university_michigan from "../assets/svg_customers/university_michigan.svg";
+import nasa from "../assets/svg_customers/nasa.svg";
+import ford from "../assets/svg_customers/ford.svg";
+import sap from "../assets/svg_customers/sap.svg";
+import ge from "../assets/svg_customers/ge.svg";
+import google from "../assets/svg_customers/google.svg";
+import kroger from "../assets/svg_customers/kroger.svg";
+import cvs_health from "../assets/svg_customers/cvs_health.svg";
+import critical_mass from "../assets/svg_customers/critical_mass.svg";
+import salesforce from "../assets/svg_customers/salesforce.svg";
+import deloitte from "../assets/svg_customers/deloitte.svg";
+import ibm from "../assets/svg_customers/ibm.svg";
+import dropbox from "../assets/svg_customers/dropbox.svg";
+import oscar from "../assets/svg_customers/oscar.svg";
+import pfizer from "../assets/svg_customers/pfizer.svg";
+import microsoft from "../assets/svg_customers/microsoft.svg";
+import volkswagen from "../assets/svg_customers/volkswagen.svg";
+import union_bank from "../assets/svg_customers/union_bank.svg";
+
+
+
+const Customers = () => {
+    return (
+        <section className="customers-section">
+            <div className="logo-container">
+                <img src={charles_schwab} alt="Charles Schwab logo"/>
+                <img src={us_bank} alt="US Bank logo"/>
+                <img src={united_airlines} alt="United Airlines logo"/>
+                <img src={shopify} alt="Shopify logo"/>
+                <img src={mission_lane} alt="Mission Lane logo"/>
+                <img src={american_express} alt="American Express logo"/>
+                <img src={fedex} alt="FedEx logo"/>
+                <img src={university_michigan} alt="University of Michigan logo"/>
+                <img src={nasa} alt="NASA logo"/>
+                <img src={ford} alt="Ford logo"/>
+                <img src={sap} alt="SAP logo"/>
+                <img src={ge} alt="GE logo"/>
+                <img src={google} alt="Google logo"/>
+                <img src={kroger} alt="Kroger logo"/>
+                <img src={cvs_health} alt="CVS Health logo"/>
+                <img src={critical_mass} alt="Critical Mass logo"/>
+                <img src={salesforce} alt="Salesforce logo"/>
+                <img src={deloitte} alt="Deloitte logo"/>
+                <img src={ibm} alt="IBM logo"/> 
+                <img src={dropbox} alt="Dropbox logo"/>
+                <img src={oscar} alt="Oscar logo"/>
+                <img src={pfizer} alt="Pfizer logo"/>
+                <img src={microsoft} alt="Microsoft logo"/>
+                <img src={volkswagen} alt="Volkswagen logo"/>
+                <img src={union_bank} alt="Union Bank logo"/>
+
+                <img src={charles_schwab} alt="Charles Schwab logo"/>
+                <img src={us_bank} alt="US Bank logo"/>
+                <img src={united_airlines} alt="United Airlines logo"/>
+                <img src={shopify} alt="Shopify logo"/>
+                <img src={mission_lane} alt="Mission Lane logo"/>
+                <img src={american_express} alt="American Express logo"/>
+                <img src={fedex} alt="FedEx logo"/>
+                <img src={university_michigan} alt="University of Michigan logo"/>
+                <img src={nasa} alt="NASA logo"/>
+                <img src={ford} alt="Ford logo"/>
+                <img src={sap} alt="SAP logo"/>
+                <img src={ge} alt="GE logo"/>
+                <img src={google} alt="Google logo"/>
+                <img src={kroger} alt="Kroger logo"/>
+                <img src={cvs_health} alt="CVS Health logo"/>
+                <img src={critical_mass} alt="Critical Mass logo"/>
+                <img src={salesforce} alt="Salesforce logo"/>
+                <img src={deloitte} alt="Deloitte logo"/>
+                <img src={ibm} alt="IBM logo"/> 
+                <img src={dropbox} alt="Dropbox logo"/>
+                <img src={oscar} alt="Oscar logo"/>
+                <img src={pfizer} alt="Pfizer logo"/>
+                <img src={microsoft} alt="Microsoft logo"/>
+                <img src={volkswagen} alt="Volkswagen logo"/>
+                <img src={union_bank} alt="Union Bank logo"/>
+            </div>
+        </section>
+    )
+}
+
+export default Customers;
