e9dd9f6699fa1f28520eac3dfa6d6c731263d86e
naiiiden <na4ko359@gmail.com>
2022-11-03 13:57:20 +0200

    show video after clicking on placeholder play button

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -14,13 +14,13 @@ const Video = () => {
                 </h2>
                 <img src={yellow_bg} alt=""/>
             </div>
-            <div className="video-container" style={{overflow: "unset",}}>
+            <div className="video-container">
                 <div className="video-subcontainer">
                     <img src={video_placeholder} alt="" className="placeholder"/>
-                    <img src={play} alt="" className="play"/>
+                    <img src={play} alt="" className="play" onClick={(e) => e.target.parentNode.style.display = "none"}/>
                 </div>
-                {/* embed youtube video after styling */}
-                <iframe src="https://www.youtube.com/embed/_SMRBDaXq84?autoplay=1&mute=1" width="100%" height="100%" frameBorder="0"/>
+                {/* embed youtube video after styling  ?autoplay=1&mute=1*/}
+                <iframe src="https://www.youtube.com/embed/_SMRBDaXq84" width="100%" height="100%" frameBorder="0"/>
             </div>
             <p>More than 40,000 designers, developers, product managers, and accessibility experts from over 12,000 companies use Stark’s suite of integrated accessibility tools in their daily product development cycle. Join them today!</p>
         </section>
