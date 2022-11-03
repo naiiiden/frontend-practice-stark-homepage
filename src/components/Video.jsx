471049e42dcfb565caa348b5615ef69810fd4b93
naiiiden <na4ko359@gmail.com>
2022-11-03 14:46:39 +0200

    video now plays with sound on

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -21,7 +21,7 @@ const Video = () => {
                     <img src={video_placeholder} alt="" className="placeholder"/>
                     <img src={play} alt="" className="play" onClick={(e) => (e.target.parentNode.style.display = "none", setPlayVideo(true))}/>
                 </div>
-                {playVideo && <iframe src="https://www.youtube.com/embed/_SMRBDaXq84?autoplay=1&mute=1" width="100%" height="100%" frameBorder="0" allow="autoplay"/>}
+                {playVideo && <iframe src="https://www.youtube.com/embed/_SMRBDaXq84?autoplay=1&mute=0" width="100%" height="100%" frameBorder="0" allow="autoplay"/>}
             </div>
             <p>More than 40,000 designers, developers, product managers, and accessibility experts from over 12,000 companies use Stark’s suite of integrated accessibility tools in their daily product development cycle. Join them today!</p>
         </section>
