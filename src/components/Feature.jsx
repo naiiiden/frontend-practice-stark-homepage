9955beb69953e3e9166d91b805d95bb9cdf9530d
naiiiden <na4ko359@gmail.com>
2022-11-05 20:22:01 +0200

    feature component add additional wrapper div

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,18 +1,20 @@
 const Feature = ({ sectionClass, mt, pShort, dec, h2, desc, href, aText, imgClass, src1, src2 }) => {
     return (
         <section className={`feature-section ${sectionClass}`} style={{"margin-top": `${mt}px`}}>
-            <div className="text-container">
-                <div className="decoration-container">
-                    <p>{pShort}</p>
-                    {dec && <img src={dec} alt=""/>}
+            <div className="inner-wrapper-div">
+                <div className="text-container">
+                    <div className="decoration-container">
+                        <p>{pShort}</p>
+                        {dec && <img src={dec} alt=""/>}
+                    </div>
+                    <h2>{h2}</h2>
+                    <p>{desc}</p>
+                    <a href={href}>{aText}</a>
                 </div>
-                <h2>{h2}</h2>
-                <p>{desc}</p>
-                <a href={href}>{aText}</a>
-            </div>
-            <div className={`image-container ${imgClass}`}>
-                {src1 && <img src={src1} alt=""/>}
-                {src2 && <img src={src2} alt=""/>}
+                <div className={`image-container ${imgClass}`}>
+                    {src1 && <img src={src1} alt=""/>}
+                    {src2 && <img src={src2} alt=""/>}
+                </div>
             </div>
         </section>
     )
