da2cf3a28b2a853506f0c556862a1740ecd37e7f
naiiiden <na4ko359@gmail.com>
2022-11-21 19:09:28 +0200

    step component add inner wrapper div

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -3,9 +3,11 @@ import PurpleLink from "./PurpleLink"
 const Step = ({ step, h3, desc, linkText, href }) => {
     return (
         <div className="step-container">
-            <p className="step">{step}</p>
-            <h3>{h3}</h3>
-            <p className="desc">{desc}</p>
+            <div>
+                <p className="step">{step}</p>
+                <h3>{h3}</h3>
+                <p className="desc">{desc}</p>
+            </div>
             <PurpleLink text={linkText} href={href}/>
         </div>
     )
