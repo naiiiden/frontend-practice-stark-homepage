f124c197f9bf33adefcde57f131e2d74346135ca
naiiiden <na4ko359@gmail.com>
2022-11-23 08:53:13 +0200

    features2.jsx add headinglevel2 component

--- src/components/Features2.jsx
+++ src/components/Features2.jsx
@@ -1,18 +1,16 @@
 import red_bg from "../assets/svg/red-bg.svg";
 import sparkle from "../assets/svg/sparkle.svg";
+import HeadingLevel2 from "./HeadingLevel2";
 
 const Features2 = () => {
     return (
         <section className="features2-section">
-            <div className="h2-svg-container">
-                <h2>
-                    Come for the features,
-                    <br/>
-                    stay for the magic
-                </h2>
-                <img src={red_bg} alt=""/>
-                <img src={sparkle} alt="" />
-            </div>
+            <HeadingLevel2 
+                headingLine1="Come for the features,"
+                headingLine2="stay for the magic"
+                img1={red_bg}
+                img2={sparkle}
+            />
         </section>
     )
 }
