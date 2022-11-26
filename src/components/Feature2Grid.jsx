9b6e4a2b3dcdc7e42d051c798dfdf985f93b3f73
naiiiden <na4ko359@gmail.com>
2022-11-26 22:51:05 +0200

    feature2 style wrapper divs

--- src/components/Feature2Grid.jsx
+++ src/components/Feature2Grid.jsx
@@ -18,57 +18,59 @@ import blob2 from "../assets/images_homepage/homepage-blob-2.png";
 
 const Feature2Grid = ({}) => {
     return (
-        <div className="grid-bg-wrapper">
-            <img src={blob1} alt=""/>
-            <img src={blob2} alt=""/>
-            <div className="features-grid">
-                <div className="item contrast-checker">
-                    <img src={contrast} alt=""/>
-                    <p>CONTRAST CHECKER + COLOR SUGGESTIONS</p>
-                </div>
-                <div className="item focus-order">
-                    <img src={focus_order} alt=""/>
-                    <p>FOCUS ORDER</p>
-                </div>
-                <div className="item landmarks">
-                    <p>LANDMARKS</p>
-                    <img src={landmarks} alt=""/>
-                </div>
-                <div className="item vision-simulator">
-                    <p>VISION SIMULATOR + GENERATOR</p>
-                    <img src={vision_simulator} alt=""/>
-                </div>
-                <div className="item touch-targets">
-                    <img src={touch_target} alt=""/>
-                    <p>TOUCH TARGETS</p>
-                </div>
-                <div className="item works">
-                    <img src={works} alt=""/>
-                    <p>WORKS IN YOUR DESIGN TOOL AND BROWSER</p>
-                </div>
-                <div className="item education">
-                    <p>IN-APP EDUCATION</p>
-                    <img src={education} alt=""/>
-                </div>
-                <div className="item alt-text">
-                    <img src={alt_text} alt=""/>
-                    <p>ALT-TEXT ANNOTATIONS</p>
-                </div>
-                <div className="item live-preview">
-                    <p>LIVE PREVIEW IN BROWSER</p>
-                    <img src={live_preview} alt=""/>
-                </div>
-                <div className="item typography">
-                    <img src={typography} alt=""/>
-                    <p>TYPOGRAPHY</p>
-                </div>
-                <div className="item sso">
-                    <img src={sso} alt=""/>
-                    <p>SSO + TEAM ACCOUNT MANAGEMENT</p>
-                </div>
-                <div className="item fast">
-                    <p>BEAUTIFULLY SIMPLE, LIGHTNING FAST</p>
-                    <img src={fast} alt=""/>
+        <div className="outer-wrapper-div">
+            <div className="inner-wrapper-div">
+                {/* <img src={blob1} alt="" className="blob1"/> */}
+                {/* <img src={blob2} alt="" className="blob2"/> */}
+                <div className="features-grid">
+                    <div className="item contrast-checker">
+                        <img src={contrast} alt=""/>
+                        <p>CONTRAST CHECKER + COLOR SUGGESTIONS</p>
+                    </div>
+                    <div className="item focus-order">
+                        <img src={focus_order} alt=""/>
+                        <p>FOCUS ORDER</p>
+                    </div>
+                    <div className="item landmarks">
+                        <p>LANDMARKS</p>
+                        <img src={landmarks} alt=""/>
+                    </div>
+                    <div className="item vision-simulator">
+                        <p>VISION SIMULATOR + GENERATOR</p>
+                        <img src={vision_simulator} alt=""/>
+                    </div>
+                    <div className="item touch-targets">
+                        <img src={touch_target} alt=""/>
+                        <p>TOUCH TARGETS</p>
+                    </div>
+                    <div className="item works">
+                        <img src={works} alt=""/>
+                        <p>WORKS IN YOUR DESIGN TOOL AND BROWSER</p>
+                    </div>
+                    <div className="item education">
+                        <p>IN-APP EDUCATION</p>
+                        <img src={education} alt=""/>
+                    </div>
+                    <div className="item alt-text">
+                        <img src={alt_text} alt=""/>
+                        <p>ALT-TEXT ANNOTATIONS</p>
+                    </div>
+                    <div className="item live-preview">
+                        <p>LIVE PREVIEW IN BROWSER</p>
+                        <img src={live_preview} alt=""/>
+                    </div>
+                    <div className="item typography">
+                        <img src={typography} alt=""/>
+                        <p>TYPOGRAPHY</p>
+                    </div>
+                    <div className="item sso">
+                        <img src={sso} alt=""/>
+                        <p>SSO + TEAM ACCOUNT MANAGEMENT</p>
+                    </div>
+                    <div className="item fast">
+                        <p>BEAUTIFULLY SIMPLE, LIGHTNING FAST</p>
+                        <img src={fast} alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
