598a07a2aa654b922e3144cca0c348bc4883c369
naiiiden <na4ko359@gmail.com>
2022-11-30 17:46:21 +0200

    footer set margin/padding/bg & add inner wrapper div for max-width on desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,59 +13,61 @@ import meetup from "../assets/images_footer/footer__meetup.png";
 const Footer = () => {
     return (
         <footer>
-            <div className="heading-logo-wrapper">
-                <HeadingLevel2 headingLine1="Made remotely with love by Stark Lab, Inc."/>
-                <img src={logo} alt=""/>
-            </div>
-            <nav aria-label="Footer">
-                <ul className="nav-ul">
-                    <li><h3 className="uppercase">Product</h3></li>
-                    <li><a href="">Stark for Mac</a></li>
-                    <li><a href="">Stark for Figma</a></li>
-                    <li><a href="">Stark for Sketch</a></li>
-                    <li><a href="">Stark for Adobe XD</a></li>
-                    <li><a href="">Stark for Chrome</a></li>
-                    <li><a href="">Stark for Edge</a></li>
-                    <li><a href="">Stark for Safari</a></li>
-                    <li><a href="">Stark for Arc</a></li>
-                    <li><a href="">Stark for Brave</a></li>
-                </ul>
-                <ul className="nav-ul">
-                    <li><h3 className="uppercase">Resources</h3></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Library</a></li>
-                    <li><a href="">Release Notes</a></li>
-                </ul>
-                <ul className="nav-ul">
-                    <li><h3 className="uppercase">Help</h3></li>
-                    <li><a href="">My Account</a></li>
-                    <li><a href="">Support Docs</a></li>
-                    <li><a href="">Contact Us</a></li>
-                    <li><a href="">Feature Requests</a></li>
-                </ul>
-                <ul className="nav-ul">
-                    <li><h3 className="uppercase">Company</h3></li>
-                    <li><a href="">Press Releases</a></li>
-                    <li><a href="">Privacy Policy</a></li>
-                    <li><a href="">Cookie Policy</a></li>
-                    <li><a href="">Security Overview</a></li>
-                    <li><a href="">Status</a></li>
-                    <li><a href="">Terms of Service</a></li>
-                    <li><a href="">Code of Conduct</a></li>
-                </ul>
-            </nav>
-            <div className="socials-copyright-wrapper">
-                <nav aria-label="Socials">
-                    <ul className="socials">
-                        <li><img src={twitter} alt="" /></li>
-                        <li><img src={instagram} alt="" /></li>
-                        <li><img src={slack} alt="" /></li>
-                        <li><img src={youtube} alt="" /></li>
-                        <li><img src={dribbble} alt="" /></li>
-                        <li><img src={meetup} alt="" /></li>
+            <div className="footer-inner-wrapper">
+                <div className="heading-logo-wrapper">
+                    <HeadingLevel2 headingLine1="Made remotely with love by Stark Lab, Inc."/>
+                    <img src={logo} alt=""/>
+                </div>
+                <nav aria-label="Footer">
+                    <ul className="nav-ul">
+                        <li><h3 className="uppercase">Product</h3></li>
+                        <li><a href="">Stark for Mac</a></li>
+                        <li><a href="">Stark for Figma</a></li>
+                        <li><a href="">Stark for Sketch</a></li>
+                        <li><a href="">Stark for Adobe XD</a></li>
+                        <li><a href="">Stark for Chrome</a></li>
+                        <li><a href="">Stark for Edge</a></li>
+                        <li><a href="">Stark for Safari</a></li>
+                        <li><a href="">Stark for Arc</a></li>
+                        <li><a href="">Stark for Brave</a></li>
+                    </ul>
+                    <ul className="nav-ul">
+                        <li><h3 className="uppercase">Resources</h3></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Library</a></li>
+                        <li><a href="">Release Notes</a></li>
+                    </ul>
+                    <ul className="nav-ul">
+                        <li><h3 className="uppercase">Help</h3></li>
+                        <li><a href="">My Account</a></li>
+                        <li><a href="">Support Docs</a></li>
+                        <li><a href="">Contact Us</a></li>
+                        <li><a href="">Feature Requests</a></li>
+                    </ul>
+                    <ul className="nav-ul">
+                        <li><h3 className="uppercase">Company</h3></li>
+                        <li><a href="">Press Releases</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                        <li><a href="">Cookie Policy</a></li>
+                        <li><a href="">Security Overview</a></li>
+                        <li><a href="">Status</a></li>
+                        <li><a href="">Terms of Service</a></li>
+                        <li><a href="">Code of Conduct</a></li>
                     </ul>
                 </nav>
-                <p className="uppercase">Stark Lab, Inc. Copyright 2022</p>
+                <div className="socials-copyright-wrapper">
+                    <nav aria-label="Socials">
+                        <ul className="socials">
+                            <li><img src={twitter} alt="" /></li>
+                            <li><img src={instagram} alt="" /></li>
+                            <li><img src={slack} alt="" /></li>
+                            <li><img src={youtube} alt="" /></li>
+                            <li><img src={dribbble} alt="" /></li>
+                            <li><img src={meetup} alt="" /></li>
+                        </ul>
+                    </nav>
+                    <p className="uppercase">Stark Lab, Inc. Copyright 2022</p>
+                </div>
             </div>
         </footer>
     )
