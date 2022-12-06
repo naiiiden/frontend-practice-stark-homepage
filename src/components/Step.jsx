018935d13a6c9d82c5f3654de4cee90a5958de92
naiiiden <na4ko359@gmail.com>
2022-12-06 23:30:49 +0200

    step.jsx download_mac

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,6 +1,10 @@
+import { useLocation } from "react-router-dom";
 import PurpleLink from "./PurpleLink"
+import download_mac from "../assets/images_repeating/download-mac.svg";
 
-const Step = ({ step, h3, desc, linkText, href }) => {
+const Step = ({ step, h3, desc, linkText, href, macTrue }) => {
+    const { pathname } = useLocation();
+
     return (
         <div className="step-container">
             <div>
@@ -8,7 +12,11 @@ const Step = ({ step, h3, desc, linkText, href }) => {
                 <h3>{h3}</h3>
                 <p className="desc">{desc}</p>
             </div>
+            {pathname == "/mac" && macTrue ? 
+            <a href=""><img src={download_mac} alt="Download on the Mac App Store"/></a>
+            :
             <PurpleLink text={linkText} href={href}/>
+            }
         </div>
     )
 }
