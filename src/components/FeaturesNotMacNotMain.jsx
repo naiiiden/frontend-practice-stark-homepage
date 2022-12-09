db813675f601c219020c6f2b2c89d4e245cb9835
naiiiden <na4ko359@gmail.com>
2022-12-09 23:24:42 +0200

    hide feature4 on /adobe-xd

--- src/components/FeaturesNotMacNotMain.jsx
+++ src/components/FeaturesNotMacNotMain.jsx
@@ -44,17 +44,21 @@ const FeaturesNotMacNotMain = () => {
                 text="Get started for free"
                 href="https://account.getstark.co/"
             />
-            <Feature
-                sectionClass="feature-mac-4"
-                mt="200"
-                pShort="Retrofitting at scale with a click"
-                h2="Accessibility easy as one, two, three"
-                desc="Once you open the Stark plugin in Sketch or Figma it automagically connects to Stark for Mac. So you can apply for example color suggestions to individual design elements or rectify all instances of an issue across your entire file or design system with just a click. That’s powerful!"
-                src1={(pathname == "/figma" || pathname == "sketch") ? feature_rest_4_2 : feature_rest_4}
+            {pathname == "/adobe-xd" ? 
+                "" 
+                :
+                <Feature
+                    sectionClass="feature-mac-4"
+                    mt="200"
+                    pShort="Retrofitting at scale with a click"
+                    h2="Accessibility easy as one, two, three"
+                    desc="Once you open the Stark plugin in Sketch or Figma it automagically connects to Stark for Mac. So you can apply for example color suggestions to individual design elements or rectify all instances of an issue across your entire file or design system with just a click. That’s powerful!"
+                    src1={(pathname == "/figma" || pathname == "sketch") ? feature_rest_4_2 : feature_rest_4}
                 idImg1="feature-rest-img4"
                 text="Get started for free"
                 href="https://account.getstark.co/"
             />
+            }
         </div>
     )
 }
