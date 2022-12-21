fb06e30b6560634a76fefdce25f094785a8d2a77
naiiiden <na4ko359@gmail.com>
2022-12-21 10:20:12 +0200

    testimonial autoplay functionality(ty openai)

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,30 +4,41 @@ import Quotes from "../../Quotes";
 import HeadingLevel2 from "../HeadingLevel2/HeadingLevel2";
 import Testimonial from "./Testimonial";
 
+import { useState, useEffect } from 'react';
+
 const Testimonials = () => {
-
-    const userTestimonials = Quotes.map(testimonial => {
-        return <Testimonial 
-                    key={testimonial.id} 
-                    src={testimonial.img} 
-                    name={testimonial.name} 
-                    job={testimonial.job} 
-                    quotation={testimonial.quotation}
-                />
-    });
-
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const userTestimonials = Quotes.map(testimonial => {
     return (
-        <section className="testimonial-section">
-            <HeadingLevel2
-                headingLine1="Loved by the community"
-                headingLine2="trusted by 8,000+ companies"
-                img1={yellow_bg2}
-            />
-            <div className="testimonials-container">
-                {userTestimonials}
-            </div>
-        </section>
-    )
-}
+      <Testimonial
+        key={testimonial.id}
+        src={testimonial.img}
+        name={testimonial.name}
+        job={testimonial.job}
+        quotation={testimonial.quotation}
+      />
+    );
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % userTestimonials.length);
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <section className="testimonial-section">
+      <HeadingLevel2
+        headingLine1="Loved by the community"
+        headingLine2="trusted by 8,000+ companies"
+        img1={yellow_bg2}
+      />
+      <div className="testimonials-container">
+        {userTestimonials[currentIndex]}
+      </div>
+    </section>
+  );
+};
 
 export default Testimonials;
