cbbf17c588c2f3ccdc6001b44d1344062eb92f37
naiiiden <na4ko359@gmail.com>
2022-12-22 21:33:35 +0200

    nav.jsx fix???

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,10 +9,10 @@ import { Link } from "react-router-dom";
 const Nav = ({ purpleBg }) => {
     const [openMenu, setOpenMenu] = useState(false);
     const [openDropdown, setOpenDropdown] = useState(false);
-    const [isDesktop, setIsDesktop] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
 
     const updateMedia = () => setIsDesktop(window.innerWidth >= 1024);
-
+    
     useEffect(() => {
         window.addEventListener("resize", updateMedia);
         return () => window.removeEventListener("resize", updateMedia);
@@ -27,11 +27,9 @@ const Nav = ({ purpleBg }) => {
             <Link to="/" aria-current="page" className="logo-link" onFocus={() => setOpenDropdown(false)}>
                 <img src={purpleBg ? stark_logo : stark_logo_purple} alt="Go to homepage" className="logo"/>
             </Link>
-            <ul role="menubar" className={`nav-ul ${!openMenu ? "" : "show"}`}>
+            <ul role="menubar" className={`nav-ul ${!openMenu ?  "" : "show"}`}>
                 <li role="none">
-                    {!isDesktop ? 
-                    <span className="uppercase">Product</span>
-                    :
+                    {isDesktop ? 
                     <button className="dropdown-button uppercase" role="menuitem" aria-haspopup="true" href="#product" aria-expanded={openDropdown ? "true" : "false"} onMouseEnter={() => setOpenDropdown(true)} onFocus={() => setOpenDropdown(true)}>
                         Product 
                         {purpleBg ? 
@@ -40,32 +38,34 @@ const Nav = ({ purpleBg }) => {
                             <img src={purple_arrow} alt=""/>
                         }
                     </button>
+                    :
+                    <span className="uppercase">Product</span>
                     }
                     <ul role="menu" autoFocus className={`nav-product-submenu ${openDropdown ? "show" : ""}`} aria-label="product" onMouseLeave={() => setOpenDropdown(false)}>
-                        {!isDesktop ?
-                        <li role="none"><Link to="/mac" role="menuitem">Stark for Mac</Link></li>
-                        :
+                        {isDesktop ?
                         <li role="none"><Link to="/mac" role="menuitem">Stark for Mac <img src={new_svg} alt=""/></Link></li>
+                        :
+                        <li role="none"><Link to="/mac" role="menuitem">Stark for Mac</Link></li>
                         }
                         <li role="none"><Link to="/figma" role="menuitem">Stark For Figma</Link></li>
                         <li role="none"><Link to="/sketch" role="menuitem">Stark for Sketch</Link></li>
                         <li role="none"><Link to="/adobe-xd" role="menuitem">Stark for Adobe XD</Link></li>
                         <li role="none"><Link to="/chrome" role="menuitem">Stark for Chrome</Link></li>
                         <li role="none"><Link to="/edge" role="menuitem">Stark for Edge</Link></li>
-                        {!isDesktop ?
-                        <li role="none"><Link to="/safari" role="menuitem">Stark for Safari</Link></li>
-                        :
+                        {isDesktop ?
                         <li role="none"><Link to="/safari" role="menuitem">Stark for Safari <img src={new_svg} alt=""/></Link></li>
-                        }
-                        {!isDesktop ? 
-                        <li role="none"><Link to="/arc" role="menuitem">Stark for Arc</Link></li>
                         :
+                        <li role="none"><Link to="/safari" role="menuitem">Stark for Safari</Link></li>
+                        }
+                        {isDesktop ? 
                         <li role="none"><Link to="/arc" role="menuitem">Stark for Arc <img src={new_svg} alt=""/></Link></li>
+                        :
+                        <li role="none"><Link to="/arc" role="menuitem">Stark for Arc</Link></li>
                         }
                         <li role="none"><Link to="/brave" role="menuitem">Stark for Brave</Link></li>
                     </ul>
                 </li>
-                <li role="none"><Link to="/2" role="menuitem" href="https://www.getstark.co/pricing/" onFocus={() => setOpenDropdown(false)} className="uppercase">Pricing</Link></li>
+                <li role="none"><Link to="/2" role="menuitem" onFocus={() => setOpenDropdown(false)} className="uppercase">Pricing</Link></li>
                 <li role="none"><a role="menuitem" href="https://www.getstark.co/blog/" className="uppercase">Blog</a></li>
                 <li role="none"><a role="menuitem" href="https://www.getstark.co/library/" className="uppercase">Library</a></li>
                 <li role="none"><a role="menuitem" href="https://www.getstark.co/support/" className="uppercase">Support</a></li>
