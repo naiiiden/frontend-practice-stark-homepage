b9073f9d96f68c8c5e01dab26a1a198745c405f7
naiiiden <na4ko359@gmail.com>
2022-12-26 18:13:34 +0200

    privacy.jsx a11y update

--- src/pages/WallOfText/Privacy.jsx
+++ src/pages/WallOfText/Privacy.jsx
@@ -21,53 +21,55 @@ const Privacy = () => {
                         <p>Stark Lab, Inc. (“<strong>Stark Lab</strong>,” “<strong>we</strong>,” “<strong>us</strong>,” or “<strong>our</strong>”) has prepared this Privacy Policy to explain what personal information we collect, how we use and share that information, and your choice concerning our information practices. Through our website, software-as-a-service platform and related applications, we provide website and mobile application accessibility development tools (collectively, the “Service”).</p>
                         <p>Before using the Service or submitting any personal information to Stark Lab, please review this Privacy Policy carefully and contact us if you have any questions. By using the Service, you agree to the practices described in this Privacy Policy. If you do not agree to this Privacy Policy, please do not access the Site or otherwise use the Service.</p>
                     </div>
-                    <h2>1. Personal Information Collection</h2>
-                    <div>
-                        <h3>Personal Information We Collect From You:</h3>
-                        <ul>
-                            <li>
-                                <p><strong>Account Information</strong>: We collect certain information, including your name, email address, phone number, mailing/billing addresses, etc. when you create an account to use the Service.</p>
-                            </li>
-                            <li>
-                                <p><strong>Financial Information</strong>: Our payment processor(s) will collect the financial information necessary to process your payments, such as your payment card number and authentication details. Please note, however, that we store only a tokenized version of such information and do not maintain payment card information on our servers.</p>
-                            </li>
-                            <li>
-                                <p><strong>Communication Information</strong>: We may collect information when you contact us with questions or concerns and when you voluntarily respond to questionnaires, surveys or requests for market research seeking your opinion and feedback.</p>
-                            </li>
-                            <li>
-                                <p><strong>Social Media Information</strong>: We maintain a social media presence on platforms like Twitter and LinkedIn (“<strong>Social Media Pages</strong>”). When you interact with us on social media, we may receive personal information that you provide or make available to us based on your settings, such as your contact details. In addition, the companies that host our Social Media Pages may provide us with aggregate information and analytics regarding the use of our Social Media Pages.</p>
-                            </li>
-                        </ul>
-                        <p><strong>Personal Information We Process on Behalf of Our Customers</strong>: In order to provide the Service, we process certain information on behalf of our customers. We process that data, including any personal information contained within it, pursuant to our agreements with our customers. We have no direct relationship with the individuals whose personal information we process in the course of providing our Service to our customers. If you are such an individual and have questions about the processing of your personal information or want to exercise one of your privacy rights, please contact the customer that you interact with directly.</p>
-                        <p><strong>Internet Activity Information</strong>: When you use the Service, we may automatically log the following information:</p>
-                        <ul>
-                            <li>
-                                <p><strong>Device Information</strong>: The manufacturer and model, operating system, browser type, IP address, and unique identifiers of the device you use to access the Service. The information we collect may vary based on your device type and settings.</p>
-                            </li>
-                            <li>
-                                <p><strong>Usage Information</strong>: Information about how you use our Service, such as the types of content that you view or engage with, the features you use, the actions you take, and the time, frequency, and duration of your activities. We may collect this information using third party tools, such as Mixpanel.</p>
-                            </li>
-                            <li>
-                                <p><strong>Location Information</strong>: We may derive a rough estimate of your location from your IP address when you visit our website.</p>
-                            </li>
-                            <li>
-                                <p><strong>Email Open/Click Information</strong>: We may collect information about the date and time you open or click links in emails we send.</p>
-                            </li>
-                        </ul>
-                        <p>We may use the following technologies to collect Internet Activity Information:</p>
-                        <ul>
-                            <li>
-                                <p><strong>Cookies</strong>, which are text files stored on your device to uniquely identify your browser or to store information or settings in the browser to help you navigate between pages efficiently, remember your preferences, enable functionality, help us understand user activity and patterns, and facilitate online advertising.</p>
-                            </li>
-                            <li>
-                                <p><strong>Local storage technologies</strong>, like HTML5, that provide cookie-equivalent functionality but can store larger amounts of data, including on your device outside of your browser in connection with specific applications.</p>
-                            </li>
-                            <li>
-                                <p><strong>Web beacons</strong>, also known as pixel tags or clear GIFs, which are used to demonstrate that a webpage or email was accessed or opened, or that certain content was viewed or clicked.</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
+                    <section>
+                        <h2>1. Personal Information Collection</h2>
+                        <div>
+                            <h3>Personal Information We Collect From You:</h3>
+                            <ul>
+                                <li>
+                                    <p><strong>Account Information</strong>: We collect certain information, including your name, email address, phone number, mailing/billing addresses, etc. when you create an account to use the Service.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Financial Information</strong>: Our payment processor(s) will collect the financial information necessary to process your payments, such as your payment card number and authentication details. Please note, however, that we store only a tokenized version of such information and do not maintain payment card information on our servers.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Communication Information</strong>: We may collect information when you contact us with questions or concerns and when you voluntarily respond to questionnaires, surveys or requests for market research seeking your opinion and feedback.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Social Media Information</strong>: We maintain a social media presence on platforms like Twitter and LinkedIn (“<strong>Social Media Pages</strong>”). When you interact with us on social media, we may receive personal information that you provide or make available to us based on your settings, such as your contact details. In addition, the companies that host our Social Media Pages may provide us with aggregate information and analytics regarding the use of our Social Media Pages.</p>
+                                </li>
+                            </ul>
+                            <p><strong>Personal Information We Process on Behalf of Our Customers</strong>: In order to provide the Service, we process certain information on behalf of our customers. We process that data, including any personal information contained within it, pursuant to our agreements with our customers. We have no direct relationship with the individuals whose personal information we process in the course of providing our Service to our customers. If you are such an individual and have questions about the processing of your personal information or want to exercise one of your privacy rights, please contact the customer that you interact with directly.</p>
+                            <p><strong>Internet Activity Information</strong>: When you use the Service, we may automatically log the following information:</p>
+                            <ul>
+                                <li>
+                                    <p><strong>Device Information</strong>: The manufacturer and model, operating system, browser type, IP address, and unique identifiers of the device you use to access the Service. The information we collect may vary based on your device type and settings.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Usage Information</strong>: Information about how you use our Service, such as the types of content that you view or engage with, the features you use, the actions you take, and the time, frequency, and duration of your activities. We may collect this information using third party tools, such as Mixpanel.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Location Information</strong>: We may derive a rough estimate of your location from your IP address when you visit our website.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Email Open/Click Information</strong>: We may collect information about the date and time you open or click links in emails we send.</p>
+                                </li>
+                            </ul>
+                            <p>We may use the following technologies to collect Internet Activity Information:</p>
+                            <ul>
+                                <li>
+                                    <p><strong>Cookies</strong>, which are text files stored on your device to uniquely identify your browser or to store information or settings in the browser to help you navigate between pages efficiently, remember your preferences, enable functionality, help us understand user activity and patterns, and facilitate online advertising.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Local storage technologies</strong>, like HTML5, that provide cookie-equivalent functionality but can store larger amounts of data, including on your device outside of your browser in connection with specific applications.</p>
+                                </li>
+                                <li>
+                                    <p><strong>Web beacons</strong>, also known as pixel tags or clear GIFs, which are used to demonstrate that a webpage or email was accessed or opened, or that certain content was viewed or clicked.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+                    <section>
                         <h2>2. Personal Information Use</h2>
                         <p>We use your personal information for the following purposes:</p>
                         <p><strong>Service Delivery</strong>, including to:</p>
@@ -124,8 +126,8 @@ const Privacy = () => {
                                 <p>Prevent, identify, investigate and deter fraudulent, harmful, unauthorized, unethical or illegal activity, including cyberattacks and identity theft.</p>
                             </li>
                         </ul>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>3. Personal Information Sharing</h2>
                         <p><strong>Our Sharing</strong>: We do not sell, rent, license, or lease your personal information to third parties. However, we may share personal information with:</p>
                         <ul>
@@ -145,29 +147,29 @@ const Privacy = () => {
                                 <p><strong>Affiliates</strong>: We may share personal information with our current and future affiliates, meaning an entity that controls, is controlled by, or is under common control with Stark Lab. Our affiliates may use the personal information we share in a manner consistent with this Privacy Policy.</p>
                             </li>
                         </ul>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>4. Personal Information Retention</h2>
                         <p>We keep personal information until we no longer need it to satisfy the purposes described in this Privacy Policy and we have no legal obligation to keep it.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>5. Job Applicants</h2>
                         <p>When you apply for a job with Stark Lab, we collect the information that you provide in connection with your job application. This includes name, contact information, professional credentials and skills, educational and work history, and other information that may be included in a resume or provided during interviews (which may be recorded). This may also include demographic or diversity information that you voluntarily provide. We may also conduct background checks and receive related information.</p>
                         <p>We use applicants’ information to facilitate our recruitment activities and process employment applications, including evaluating candidates and monitoring recruitment statistics. We use successful applicants’ information to administer the employment relationship. We may also use and disclose applicants’ information (a) to improve our website, (b) as otherwise necessary to comply with relevant laws, (c) to respond to subpoenas or warrants served on Stark Lab, and (d) to protect and defend the rights or property of Stark Lab or others.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>6. Children</h2>
                         <p>Our Service is not directed to children who are under the age of 16. Stark Lab does not knowingly collect personal information from children under the age of 16. If we learn that we have collected personal information from a child under the age of 16 without the consent of the child’s parent or guardian as required by law, we will delete that information.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>7. Links to Other Websites</h2>
                         <p>The Service may contain links to other websites not operated or controlled by Stark Lab, including social media services (“<strong>Third Party Sites</strong>”). The information that you share with Third Party Sites will be governed by the specific privacy policies and terms of service of the Third Party Sites and not by this Privacy Policy. By providing these links we do not imply that we endorse or have reviewed these sites. Please contact the Third Party Sites directly for information on their privacy practices and policies.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>8. Security</h2>
                         <p>We employ a number of technical, organizational and physical safeguards designed to protect the personal information we collect. However, we cannot guarantee the security of your personal information and you use the Service at your own risk.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>9. Your Choices</h2>
                         <p><strong>Update or Correct Personal Information</strong>: You can contact us and request any updates or corrections needed to keep your personal information accurate, current, and complete.</p>
                         <p><strong>Opt Out of Marketing Communications</strong>: You may opt out of marketing communications by following the unsubscribe instructions in any marketing email we send you. Please note, however, that you may continue to receive communications as described in the Communicating with You section after opting out of marketing communications.</p>
@@ -192,17 +194,17 @@ const Privacy = () => {
                         </ul>
                         <p>Do Not Track. We currently do not respond to "Do Not Track" or similar signals. Learn more about <a href="http://www.allaboutdnt.com/">"Do Not Track"</a> here.
                         </p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>10. Changes to the Privacy Policy</h2>
                         <p>The Service and our business may change from time to time. As a result we may change this Privacy Policy at any time. When we do we will post an updated version on this page, unless another type of notice is required by applicable law. By continuing to use our Service or providing us with personal information after we have posted an updated Privacy Policy, or notified you by other means if applicable, you consent to the revised Privacy Policy and the practices described in it.</p>
-                    </div>
-                    <div>
+                    </section>
+                    <section>
                         <h2>11. Contact Us</h2>
                         <p>If you have any questions about our Privacy Policy or information practices, please feel free to contact us at our designated request address:&nbsp;
                             <a href="support@getstark.co">support@getstark.co.</a>
                         </p>
-                    </div>
+                    </section>
                 </main>
             </div>
         </>
