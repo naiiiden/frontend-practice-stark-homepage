40bdfd78ef6c473a6dfde087623040ef97c1b395
naiiiden <na4ko359@gmail.com>
2022-12-26 21:05:29 +0200

    1

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -11,54 +11,55 @@ import a4 from "../../assets/images_testimonial/homepage__avatar__carlos-torres.
 import a5 from "../../assets/images_testimonial/homepage__avatar__jon-fox.png";
 import a6 from "../../assets/images_testimonial/homepage__avatar__johnny-hill.png";
 
-const Quotes = [
-  {
-      id: 1,
-      img: a1,
-      name: "Emily Ching",
-      job: "Head of design at Making CO",
-      quotation: "Stark is my favorite plugin for Figma! I use it daily.",
-  },
-  {
-      id: 2,
-      img: a2,
-      name: "Sean Keating",
-      job: "Digital product designer at S&P Global",
-      quotation: "It's simple, easy to use, and effective. Does just what I need it to do.",
-  },
-  {
-      id: 3,
-      img: a3,
-      name: "Jordan Lucero",
-      job: "CEO at Jservices",
-      quotation: "...from the ease of use, fantastic resources, and the amount of support it gets from the team.",
-  },
-  {
-      id: 4,
-      img: a4,
-      name: "Carlos Torres",
-      job: "UX engineer at Alto",
-      quotation: "I have learned so much and how to think of others while designing.",
-  },
-  {
-      id: 5,
-      img: a5,
-      name: "Jon Fox",
-      job: "Lead UX designer at La Voting Solutions",
-      quotation: "...a welcome tool set to make interesting, impactful designs that are also accessible.",
-  },
-  {
-      id: 6,
-      img: a6,
-      name: "Johnny Hill",
-      job: "UX director at Knock, INC",
-      quotation: "Everyone should be using Stark as part of their workflow.",
-  },
-]
-
 import { useState, useEffect } from 'react';
 
 const Testimonials = () => {
+  
+  const Quotes = [
+    {
+        id: 1,
+        img: a1,
+        name: "Emily Ching",
+        job: "Head of design at Making CO",
+        quotation: "Stark is my favorite plugin for Figma! I use it daily.",
+    },
+    {
+        id: 2,
+        img: a2,
+        name: "Sean Keating",
+        job: "Digital product designer at S&P Global",
+        quotation: "It's simple, easy to use, and effective. Does just what I need it to do.",
+    },
+    {
+        id: 3,
+        img: a3,
+        name: "Jordan Lucero",
+        job: "CEO at Jservices",
+        quotation: "...from the ease of use, fantastic resources, and the amount of support it gets from the team.",
+    },
+    {
+        id: 4,
+        img: a4,
+        name: "Carlos Torres",
+        job: "UX engineer at Alto",
+        quotation: "I have learned so much and how to think of others while designing.",
+    },
+    {
+        id: 5,
+        img: a5,
+        name: "Jon Fox",
+        job: "Lead UX designer at La Voting Solutions",
+        quotation: "...a welcome tool set to make interesting, impactful designs that are also accessible.",
+    },
+    {
+        id: 6,
+        img: a6,
+        name: "Johnny Hill",
+        job: "UX director at Knock, INC",
+        quotation: "Everyone should be using Stark as part of their workflow.",
+    },
+  ]
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const userTestimonials = Quotes.map(testimonial => {
     return (
