3dfd765aed1120cdd25a503d9f75e7759a39233f
naiiiden <na4ko359@gmail.com>
2022-12-26 20:57:22 +0200

    testiomnial profile picture temporary fix

--- src/components/Testimonials/Quotes.js
+++ src/components/Testimonials/Quotes.js
@@ -1,46 +1,46 @@
-const Quotes = [
-    {
-        id: 1,
-        img: "/assets/images_testimonial/homepage__avatar__emily-ching.png",
-        name: "Emily Ching",
-        job: "Head of design at Making CO",
-        quotation: "Stark is my favorite plugin for Figma! I use it daily.",
-    },
-    {
-        id: 2,
-        img: "/assets/images_testimonial/homepage__avatar__sean-keating.png",
-        name: "Sean Keating",
-        job: "Digital product designer at S&P Global",
-        quotation: "It's simple, easy to use, and effective. Does just what I need it to do.",
-    },
-    {
-        id: 3,
-        img: "/assets/images_testimonial/homepage__avatar__jordan-lucero.png",
-        name: "Jordan Lucero",
-        job: "CEO at Jservices",
-        quotation: "...from the ease of use, fantastic resources, and the amount of support it gets from the team.",
-    },
-    {
-        id: 4,
-        img: "/assets/images_testimonial/homepage__avatar__carlos-torres.png",
-        name: "Carlos Torres",
-        job: "UX engineer at Alto",
-        quotation: "I have learned so much and how to think of others while designing.",
-    },
-    {
-        id: 5,
-        img: "/assets/images_testimonial/homepage__avatar__jon-fox.png",
-        name: "Jon Fox",
-        job: "Lead UX designer at La Voting Solutions",
-        quotation: "...a welcome tool set to make interesting, impactful designs that are also accessible.",
-    },
-    {
-        id: 6,
-        img: "/images_testimonial/homepage__avatar__johnny-hill.png",
-        name: "Johnny Hill",
-        job: "UX director at Knock, INC",
-        quotation: "Everyone should be using Stark as part of their workflow.",
-    },
-]
+// const Quotes = [
+//     {
+//         id: 1,
+//         img: "/assets/images_testimonial/homepage__avatar__emily-ching.png",
+//         name: "Emily Ching",
+//         job: "Head of design at Making CO",
+//         quotation: "Stark is my favorite plugin for Figma! I use it daily.",
+//     },
+//     {
+//         id: 2,
+//         img: "/assets/images_testimonial/homepage__avatar__sean-keating.png",
+//         name: "Sean Keating",
+//         job: "Digital product designer at S&P Global",
+//         quotation: "It's simple, easy to use, and effective. Does just what I need it to do.",
+//     },
+//     {
+//         id: 3,
+//         img: "/assets/images_testimonial/homepage__avatar__jordan-lucero.png",
+//         name: "Jordan Lucero",
+//         job: "CEO at Jservices",
+//         quotation: "...from the ease of use, fantastic resources, and the amount of support it gets from the team.",
+//     },
+//     {
+//         id: 4,
+//         img: "/assets/images_testimonial/homepage__avatar__carlos-torres.png",
+//         name: "Carlos Torres",
+//         job: "UX engineer at Alto",
+//         quotation: "I have learned so much and how to think of others while designing.",
+//     },
+//     {
+//         id: 5,
+//         img: "/assets/images_testimonial/homepage__avatar__jon-fox.png",
+//         name: "Jon Fox",
+//         job: "Lead UX designer at La Voting Solutions",
+//         quotation: "...a welcome tool set to make interesting, impactful designs that are also accessible.",
+//     },
+//     {
+//         id: 6,
+//         img: "/images_testimonial/homepage__avatar__johnny-hill.png",
+//         name: "Johnny Hill",
+//         job: "UX director at Knock, INC",
+//         quotation: "Everyone should be using Stark as part of their workflow.",
+//     },
+// ]
 
-export default Quotes;
+// export default Quotes;
