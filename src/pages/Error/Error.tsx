2e0db47345f9aa110d611e760c78f2c16912e3e7
naiiiden <na4ko359@gmail.com>
2023-05-18 07:56:37 +0300

    404 page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -7,15 +7,15 @@ const Error = () => {
     return (
         <>
             <header className="header-transparent">
-                <Nav purpleBg={undefined}/>
+                <Nav purpleBg={false}/>
             </header>
             <main className="error-page">
                 <p className="uppercase">404 Error. Whoops.</p>
                 <h1>Hmm. That didn't work.</h1>
                 <p className="desc">Maybe the link doesn’t exist. Or maybe we just broke it? Hopefully we can take you where you want to go though.</p>
                 <div className="links-wrapper">
-                  <PurpleLink href="" text="Go to Support Center" className={""} macLink={""}/>
-                  <a href="" className="contact-us-link">Contact us</a>
+                  <PurpleLink href="https://www.getstark.co/support" text="Go to Support Center"/>
+                  <a href="https://www.getstark.co/support/contact-us" className="contact-us-link">Contact us</a>
                 </div>
             </main>
         </>
